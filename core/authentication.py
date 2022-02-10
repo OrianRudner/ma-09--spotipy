eb33d4f27fd1a03a3983f5a7ae94bf5c3236da2b
OrianRudner <orian.rudner@gmail.com>
2022-02-10 11:04:43 +0200

    authntication logs

--- core/authentication.py
+++ core/authentication.py
@@ -1,6 +1,10 @@
 import json
+import logging
 from core.const import UserType
 from core.modules.regular_user import RegularUser
+
+logging.basicConfig(level=logging.INFO, filename='loggers.log',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 def read_user_file(file_path):
@@ -14,10 +18,13 @@ def log_in(user_name, password, file_path):
     for user in existing_users["users"]:
         if user["user"] == user_name and user["password"] == password:
             if user["type"] == UserType.REGULAR:
+                logging.info('regular user logged in')
                 return RegularUser(user["user"], user["user"])
             else:
                 if user["type"] == UserType.PREMIUM:
+                    logging.info('premium user logged in')
                     return RegularUser(user["user"], user["user"])
+    logging.info("user couldn't log in")
     return False
 
 
@@ -26,7 +33,9 @@ def sigh_up(user_name, password, file_path, type="regular"):
     existing_users = read_user_file(file_path)
     for user in existing_users["users"]:
         if user["user"] == user_name:
+            logging.info("user already exists")
             return
     existing_users["users"].append(new_user)
     with open(file_path, 'r+') as users_file:
         json.dump(existing_users, users_file, indent=4)
+    logging.info("new user sighed up")
